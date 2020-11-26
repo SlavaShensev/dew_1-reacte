62953acce97cdea9020b2d9cb7600245b8444a24
Slava <SlavaShensev@gmail.com>
2020-11-27 00:36:56 +0200

    'refactoring-header'

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import h from './Header.module.css';
+import logo from '../Static/facebook.png';
 
 const Header = () => {
     return (
-            <header className={h.header}>
-                <div>
-                    <img className={h.logo}
-                         src={'https://themified.com/friend-finder/images/logo.png'}/>
-                </div>
-
-            </header>
+        <div className={h.header}>
+            <div className={h.logo}>
+                <img className={h.logo_item} src={logo}/>
+            </div>
+            <div className={h.title}>
+                <h1>
+                    Closer Than We Think
+                </h1>
+            </div>
+            <div>
+                <input type={'text'} className={h.input} placeholder={'finder'}/>
+                <button className={h.button}>
+                    Search
+                </button>
+            </div>
+        </div>
     )
 };
 
