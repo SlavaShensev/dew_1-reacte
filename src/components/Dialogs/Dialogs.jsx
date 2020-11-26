c42d31117743277d9055c89982e72233d26eaa1a
Slava <SlavaShensev@gmail.com>
2020-11-26 22:23:51 +0200

    'encapsulate-dialogs'

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,38 +2,44 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
-const Dialogs = () => {
 
+const DialogItem = (props) => {
+
+    let path = '/dialogs/' + props.id;
+
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={path}> {props.name} </NavLink>
+        </div>
+    )
+};
+
+const Massage = (props) => {
+    return (
+        <div className={s.dialog}>
+            {props.massage}
+        </div>
+    )
+};
+
+const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.active}>
-                    <NavLink to={'/dialogs/1'}>Pushkin </NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to={'/dialogs/2'}>Shevchenko</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to={'/dialogs/3'}>Lomonosov</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to={'/dialogs/4'}>Kopernik</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to={'/dialogs/5'}>Enshteyn</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to={'/dialogs/6'}>Tomas Edison</NavLink>
-                </div>
+                <DialogItem name={'Shevchenko'} id={'1'}/>
+                <DialogItem name={'Lomonosov'} id={'2'}/>
+                <DialogItem name={'Lomonosov'} id={'3'}/>
+                <DialogItem name={'Kopernik'} id={'4'}/>
+                <DialogItem name={'Enshteyn'} id={'5'}/>
+                <DialogItem name={'Tomas Edison'} id={'6'}/>
+                <DialogItem name={'Goof'} id={'7'}/>
             </div>
 
             <div className="messages">
-                <div className="message">Hello!</div>
-                <div className="message">Are you programmer?</div>
-                <div className="message">Can you set up windows for my computer, please?</div>
+                <Massage massage={'Hello!'}/>
+                <Massage massage={'Are you programmer?'}/>
+                <Massage massage={'Can you set up windows for my computer, please?'}/>
             </div>
-
-
         </div>
     )
 };
