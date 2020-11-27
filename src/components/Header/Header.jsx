11972560db4e9ef85029cde92029168de77318bb
Slava <SlavaShensev@gmail.com>
2020-11-27 09:22:24 +0200

    'header-add-logo'

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import h from './Header.module.css';
-import logo from '../Static/facebook.png';
+import facebook from '../Static/facebook.png';
+import twitter from '../Static/twitter.png';
+import all from '../Static/all.png';
 
 const Header = () => {
     return (
         <div className={h.header}>
             <div className={h.logo}>
-                <img className={h.logo_item} src={logo}/>
-            </div>
-            <div className={h.title}>
-                <h1>
-                    Closer Than We Think
-                </h1>
+                <img className={h.logo_item} src={facebook}/>
+                <img className={h.logo_item} src={twitter}/>
+                <img className={h.logo_item} src={all}/>
             </div>
             <div>
                 <input type={'text'} className={h.input} placeholder={'finder'}/>
