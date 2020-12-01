67abad85458a570be9db89d022198053a0c9df7b
Slava <SlavaShensev@gmail.com>
2020-12-01 10:29:22 +0200

    'add-active-link'

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import n from './NavBar.module.css';
 
 const NavBar = () => {
     return (
         <div className={n.nav}>
-            <div className={n.profile}>
-                <a href={'/profile'}>Profile</a>
+            <div className={n.item}>
+                <NavLink to={'/profile'} activeClassName={n.active}>
+                    Profile
+                </NavLink>
             </div>
-            <div className={n.message}>
-                <a href={'/dialogs'}>Message</a>
+            <div className={n.item}>
+                <NavLink to={'/dialogs'} activeClassName={n.active}>
+                    Message
+                </NavLink>
             </div>
-            <div className={n.news}>
-                <a href={'/news'}>News</a>
+            <div className={n.item}>
+                <NavLink to={'/news'} activeClassName={n.active}>
+                    News
+                </NavLink>
             </div>
-            <div className={n.music}>
-                <a href={'/music'}>Music</a>
+            <div className={n.item}>
+                <NavLink to={'/music'} activeClassName={n.active}>
+                    Music
+                </NavLink>
             </div>
-            <div className={n.setting}>
-                <a href={'/settings'}>Setting</a>
+            <div className={n.item}>
+                <NavLink to={'/settings'} activeClassName={n.active}>
+                    Setting
+                </NavLink>
             </div>
         </div>
     )
