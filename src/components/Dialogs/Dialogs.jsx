e3db0304a5dd16549df5a5bb5b5717b5a8ed606d
Slava <SlavaShensev@gmail.com>
2020-12-01 12:03:15 +0200

    'html_dialogs'

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
-
+import {NavLink} from 'react-router-dom';
+import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.item}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Slava
+                </div>
+                <div className={s.dialog}>
+                    Vova
+                </div>
+                <div className={s.dialog}>
+                    Gena
+                </div>
+                <div className={s.dialog}>
+                    Olya
+                </div>
+                <div className={s.dialog}>
+                    Nikolay
+                </div>
+            </div>
+            <div className={s.message}>
+                <div>
+                    Hello
+                </div>
+                <div>
+                    You are you
+                </div>
+            </div>
         </div>
     )
 };
