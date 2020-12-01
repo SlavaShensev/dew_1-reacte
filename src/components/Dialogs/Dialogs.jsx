ddd040d7ad56ccaf97dd8ab2b87d34e5f97191d3
Slava <SlavaShensev@gmail.com>
2020-12-01 12:33:35 +0200

    'dialogs_html'

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,29 +5,53 @@ import s from './Dialogs.module.css';
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
-            <div className={s.item}>
-                <div className={s.dialog + ' ' + s.active}>
-                    Slava
+            <div className={s.users}>
+                <div className={s.user__item}>
+                    <NavLink to={'#'} className={s.users__link + ' ' + s.active}>
+                        <p>
+                            Slava
+                        </p>
+                    </NavLink>
                 </div>
-                <div className={s.dialog}>
-                    Vova
+                <div className={s.user__item}>
+                    <NavLink to={'#'} className={s.users__link}>
+                        <p>
+                            Vova
+                        </p>
+                    </NavLink>
                 </div>
-                <div className={s.dialog}>
-                    Gena
+                <div className={s.user__item}>
+                    <NavLink to={'#'} className={s.users__link}>
+                        <p>
+                            Gena
+                        </p>
+                    </NavLink>
                 </div>
-                <div className={s.dialog}>
-                    Olya
+                <div className={s.user__item}>
+                    <NavLink to={'#'} className={s.users__link}>
+                        <p>
+                            Olya
+                        </p>
+                    </NavLink>
                 </div>
-                <div className={s.dialog}>
-                    Nikolay
+                <div className={s.user__item}>
+                    <NavLink to={'#'} className={s.users__link}>
+                        <p>
+                            Nikolay
+                        </p>
+                    </NavLink>
                 </div>
             </div>
-            <div className={s.message}>
-                <div>
-                    Hello
+            <div className={s.messages}>
+                <div className={s.message__item}>
+                    <p>
+                        Hello
+                    </p>
                 </div>
-                <div>
-                    You are you
+                <div className={s.message__item}>
+                    <p>
+                        You are you
+                    </p>
                 </div>
             </div>
         </div>
