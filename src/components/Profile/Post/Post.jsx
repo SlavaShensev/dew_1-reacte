4b9b23cff8312e7511719c5af85fb936e386a794
Slava <SlavaShensev@gmail.com>
2020-12-03 18:42:19 +0200

    'base-of-flux'

--- src/components/Profile/Post/Post.jsx
+++ src/components/Profile/Post/Post.jsx
@@ -4,7 +4,6 @@ import PostInfo from "./PostInfo/PostInfo";
 import PostContent from "./PostContent/PoctContent";
 
 const Post = (props) => {
-
     const postsText = props.text.map(({text}) => text);
     return (
         <div className={p.postMain}>
